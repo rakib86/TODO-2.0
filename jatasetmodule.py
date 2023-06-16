c252f31e0a166f218b408287ba2a04104cf64f4b
Mayeen47 <106159557+Mayeen47@users.noreply.github.com>
2023-06-16 14:22:22 +0600

    uopaeh dsatset
    fd

---
+++ jatasetmodule.py
@@ -0,0 +1,107 @@
+try:
+    with conn.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('john@example.com', 'mypassword'))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record inserted successfully")
+finally:
+    conn.close()
+    
+    
+    try:
+    with conn.cursor() as cursor:
+        # Delete a record
+        sql = "DELETE FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('john@example.com',))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record deleted successfully")
+finally:
+    conn.close()
+    try:
+    with conn.cursor() as cursor:
+        # Update a record
+        sql = "UPDATE `users` SET `password`=%s WHERE `email`=%s"
+        cursor.execute(sql, ('newpassword', 'john@example.com'))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record updated successfully")
+finally:
+    conn.close()
+    try:
+    with conn.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('john@example.com', 'mypassword'))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record inserted successfully")
+finally:
+    conn.close()
+    
+    
+    try:
+    with conn.cursor() as cursor:
+        # Delete a record
+        sql = "DELETE FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('john@example.com',))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record deleted successfully")
+finally:
+    conn.close()
+    try:
+    with conn.cursor() as cursor:
+        # Update a record
+        sql = "UPDATE `users` SET `password`=%s WHERE `email`=%s"
+        cursor.execute(sql, ('newpassword', 'john@example.com'))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record updated successfully")
+finally:
+    conn.close()
+    try:
+    with conn.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('john@example.com', 'mypassword'))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record inserted successfully")
+finally:
+    conn.close()
+    
+    
+    try:
+    with conn.cursor() as cursor:
+        # Delete a record
+        sql = "DELETE FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('john@example.com',))
+
+    # Commit changes
+    conn.commit()
+
+    print("Record deleted successfully")
+finally:
+    conn.close()
+    try:
+    with conn.cursor() as cursor:
+        # Update a record
+        sql = "UPDATE `users` SET `password`=%s WHERE `email`=%s"
+   
